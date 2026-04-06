d12dda4589354840121ee70a1c096a8dd8e4174a
akulasathvik7 <akulasathvik7@gmail.com>
2026-04-06 21:12:21 +0530

    modules

--- advance.py
+++ advance.py
@@ -1,6 +1,22 @@
-import index1
+# import index1
 
-print(index1.add(5, 3)) 
-print(index1.subtract(5, 3))  
-print(index1.multiply(5, 3))  
-print(index1.divide(5, 3))  
+# print(index1.add(5, 3)) 
+# print(index1.subtract(5, 3))  
+# print(index1.multiply(5, 3))  
+# print(index1.divide(5, 3))  
+
+# import keyword
+# print("The list of keywords are : ")
+# print(keyword.kwlist)
+from collections import Counter
+
+mylist="aaabbbbacccbdd"
+
+my_counter=Counter(mylist)
+# print(my_counter.values())
+# print(my_counter.keys())
+# print(my_counter.items())
+# print(list(my_counter.items()))
+# print(my_counter.most_common(1))
+# print(my_counter.most_common(1)[0])
+# print(my_counter.most_common(1)[0][0])
